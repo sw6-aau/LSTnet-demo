5c15de17c5104de300ff4136d0dc9c77a89a47b5
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2020-03-30 02:00:47 +0200

    Tune v1.1.0

--- main.py
+++ main.py
@@ -161,7 +161,7 @@ search_space = {
 #Uncomment this to limit cores/gpu utilization
 #ray.init(num_cpus=<int>, num_gpus=<int>)
 
-analysis = tune.run(tuned_train, config=search_space)
+analysis = tune.run(tuned_train, scheduler=ASHAScheduler, num_samples=30 ,config=search_space)
 
 ''' Old training code below
 # At any point you can hit Ctrl + C to break out of training early.
