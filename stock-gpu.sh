322ab912ed9d3620b8a197ea6f2ecd391efea1f2
gamped <mgampe17@student.aau.dk>
2020-04-09 14:00:54 +0200

    Specify which GPU to use

--- stock-gpu.sh
+++ stock-gpu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python2 main.py --gpu  --data data/exchange_rate.txt --save save/exchange_rate.pt --hidCNN 50 --hidRNN 50 --L1Loss False --output_fun None
+python2 main.py --gpu 0 --data data/exchange_rate.txt --save save/exchange_rate.pt --hidCNN 50 --hidRNN 50 --L1Loss False --output_fun None
