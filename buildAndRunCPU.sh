de2a673a903a6a8569b9ba9f10489f0c4132966c
gamped <mgampe17@student.aau.dk>
2020-04-09 13:48:25 +0200

    Minor changes to buildAndRun scripts

--- buildAndRunCPU.sh
+++ buildAndRunCPU.sh
@@ -3,6 +3,6 @@ echo -e "\e[38;5;202m-===CLEANING===-\n\e[0m"
 docker stop lstDockCPU
 docker rm lstDockCPU
 echo -e "\n\e[38;5;4m-===BUILDING===-\n\e[0m"
-sudo docker build -t lst-cpu ./dockerfiles/gpu
+docker build -t lst-cpu ./dockerfiles/cpu
 echo -e "\n\e[38;5;2m-===RUNNING===-\n\e[0m"
-sudo docker run --gpus all -it --name lstDockCPU lst-cpu
+docker run -it --name lstDockCPU lst-cpu
--- buildAndRunGPU.sh
+++ buildAndRunGPU.sh
@@ -3,6 +3,6 @@ echo -e "\e[38;5;202m-===CLEANING===-\n\e[0m"
 docker stop lstDockGPU
 docker rm lstDockGPU
 echo -e "\n\e[38;5;4m-===BUILDING===-\n\e[0m"
-sudo docker build -t lst-gpu ./dockerfiles/gpu
+docker build -t lst-gpu ./dockerfiles/gpu
 echo -e "\n\e[38;5;2m-===RUNNING===-\n\e[0m"
-sudo docker run --gpus all -it --name lstDockGPU lst-gpu
+docker run --gpus all -it --name lstDockGPU lst-gpu
