c210e83c8c4d992304937e9348561501a9f469f6
gamped <mgampe17@student.aau.dk>
2020-04-09 13:33:08 +0200

    Added suport for GPU and dockerfile

--- stock-gpu.sh
+++ stock-gpu.sh
@@ -1,1 +1,2 @@
-python2 main.py --gpu 3 --data data/exchange_rate.txt --save save/exchange_rate.pt --hidCNN 50 --hidRNN 50 --L1Loss False --output_fun None
+#!/bin/bash
+python2 main.py --gpu  --data data/exchange_rate.txt --save save/exchange_rate.pt --hidCNN 50 --hidRNN 50 --L1Loss False --output_fun None
