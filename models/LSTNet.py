1b250ec602306a0212757f8c25b9d648241adcb0
sava17 <37406543+sava17@users.noreply.github.com>
2020-04-13 03:47:14 +0200

    Har tilføjet conv layers encoding og decoding

--- models/LSTNet.py
+++ models/LSTNet.py
@@ -15,7 +15,13 @@ class Model(nn.Module):
         self.skip = args.skip;
         self.pt = (self.P - self.Ck)/self.skip
         self.hw = args.highway_window
-        self.conv1 = nn.Conv2d(1, self.hidC, kernel_size = (self.Ck, self.m)); #In channel, out channel, kernel size (1, 50, (6, 8))
+        
+        self.conv1 = nn.Conv2d(1, self.hidC, kernel_size = (self.Ck, self.m));
+        
+        self.encode = nn.Conv2d(1, self.hidC, kernel_size = (self.Ck, self.m));
+        self.decode = nn.ConvTranspose2d(self.hidC, 1, (self.Ck, self.m))
+        
+        
         self.GRU1 = nn.GRU(self.hidC, self.hidR);
         self.dropout = nn.Dropout(p = args.dropout);
         if (self.skip > 0):
@@ -31,31 +37,53 @@ class Model(nn.Module):
         if (args.output_fun == 'tanh'):
             self.output = F.tanh;
  
+
+
+        # New convolutional layer
+
+        #self.decoder1 = nn.ConvTranspose2d(self.hidC, self.hidC, kernel_size = (self.Ck, self.m))
+
     def forward(self, x):
         batch_size = x.size(0);
+
+
+        c = x.view(-1, 1, self.P, self.m);
         
+        print("BEFORE: ")
+        print(c.shape)
+        # CNN Autoencoder
+        c = F.relu(self.encode(c))
+        c = F.relu(self.decode(c))
         #CNN
-        c = x.view(-1, 1, self.P, self.m);      #(128, 1, 168, 8) change x shape to: -1 = take whatever previous shape (this shape will vary), 1, self.P = 168 (P-window), m = 8 (columns) 
-        #print(c.shape)
-        # Hypothesis: the second argument in x is read as output channel, which will be the input channel for the next layer, which is why conv1, takes 1 as input layer.
-        #print(c.shape) 
-        c = F.relu(self.conv1(c));  
-        # conv layer setting:   (1, 50, (6,8)), 
-        # input:                (128, 1, 168, 8)
-        # output                (128, 50, 163, 1)
-        # Hypothesis: after being in conv layer that takes input size 1 it outputtet a layer with size 50. 163, 1 tho?      
-        c = self.dropout(c);        # Dropout, does not change the shape
-        #print(c.shape)
-        c = torch.squeeze(c, 3);    # (128, 50, 163) Fourth dimension "1" is removed (or 3 if you count from 0)
-        
+        c = F.relu(self.conv1(c));
+        print("AFTER: ")
+        print(c.shape)
+        #c = F.relu(self.decode(c))
+        # Dropout and squeeze
+        c = self.dropout(c);
+        print("after dropout")
+        print(c.shape)
+        c = torch.squeeze(c, 3);   #This will only squeeze 1's so when you add deconv layer the fourth dim returns to being 8.
+                                   #Thought: This implies that conv layer flattens the last dim to be 1 instead of 8.
+
         # RNN 
-        r = c.permute(2, 0, 1).contiguous();    # #163, 128, 50 Permute: Returns a view of the original tensor with its dimensions permuted 
-        # (2, 0, 1) Column 2 is no the first column, column 0 is the second and 1 is the third.
-        #When you call contiguous() , it actually makes a copy of tensor so the order of elements would be same as if tensor of same shape created from scratch. 
-        #Normally you don't need to worry about this. If PyTorch expects contiguous tensor but if its not then you will get
-        # RuntimeError: input is not contiguous and then you just add a call to contiguous().
-        print(r.shape) 
-        _, r = self.GRU1(r);    #GRU layer expects input HidC = 50, HidR = 50
+        print("Shape RNN")
+        r = c.permute(2, 0, 1).contiguous();        # We could simply not permute and feed the RNN the original shape of the data, why do they permute? Is the 
+                                                    # order the input is fed to the RNN somehow important?
+                                                    # The order was changed from (128, 50, 163) to (163, 128, 50)
+                                                    # parameters from doc: input_size, hidden_size, num_layers
+                                                    # num_layers = 50 output layers from CNN layer
+                                                    # 128 should be the batch size, but is put in as hidden_size, makes sense if you consider that each time series step in the batch
+                                                    # must have its own hidden layer representation
+                                                    # num layers = convoluted(self.P) (P = window, P from report i.e. look back horizon) default without conv 24*7 = 168
+                                                    # How is it that conv layer changes (128, 1, 168, 8) to (128, 50, 163, 1)
+                                                    # In theory permuting the original input to (128, 8, 168) should be legal. It would mean it takes the 8 input layers, 
+                                                    # of the 50 from the convolutional network, but i dont know what effect that will have. You could also apply a linear layer
+                                                    # that changes the input 8 layer to a 50 layer, like we did with linear encoding, but again not sure of the effect. 
+                                                    # In theory it should be perfectly fine as this is how normal neural networks are made, input layer (typically 
+                                                    # 1 if not multivariant like ours), hidden layers, and output layers (typically same amount as input layers)
+        print(r.shape)
+        _, r = self.GRU1(r);
         r = self.dropout(torch.squeeze(r,0));
 
         
@@ -65,7 +93,7 @@ class Model(nn.Module):
             s = c[:,:, int(-self.pt * self.skip):].contiguous();
             s = s.view(batch_size, self.hidC, self.pt, self.skip);
             s = s.permute(2,0,3,1).contiguous();
-            s = s.view(self.pt, batch_size * self.skip, self.hidC);7
+            s = s.view(self.pt, batch_size * self.skip, self.hidC);
             _, s = self.GRUskip(s);
             s = s.view(batch_size, self.skip * self.hidS);
             s = self.dropout(s);
@@ -84,7 +112,6 @@ class Model(nn.Module):
         if (self.output):
             res = self.output(res);
         return res;
-    
-        
+     
         
         
