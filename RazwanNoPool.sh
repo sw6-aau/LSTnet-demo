60a0dbacfffa9a8bd058d5bd6a027125517d69a7
sava17 <37406543+sava17@users.noreply.github.com>
2020-05-04 16:18:42 +0200

    Update RazwanNoPool.sh

--- RazwanNoPool.sh
+++ RazwanNoPool.sh
@@ -1,1 +1,1 @@
-python2 RazwanNoPool_main.py --data data/random.txt --save save/exchange_rate.pt --hidCNN 50 --hidRNN 50 --L1Loss False --output_fun None --epochs 1000
+python2 RazwanNoPool_main.py --data data/exchange_rate.txt --save save/exchange_rate.pt --hidCNN 50 --hidRNN 50 --L1Loss False --output_fun None --epochs 1000
