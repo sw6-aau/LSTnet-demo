8c48f416273763060632f4ae736b6c3523597934
Martin <36505224+OlympicalStoner@users.noreply.github.com>
2020-05-06 15:16:33 +0200

    Proper model loading in predictors

--- predict.py
+++ predict.py
@@ -136,10 +136,8 @@ optim = Optim.Optim(
 # Load the best saved model.
 # Have changed the train- and validation to 0, so it testes on all the data
 
-with open(args.save, 'rb+') as f:
-    checkpoint = torch.load(f)
-model.load_state_dict(checkpoint['model_state_dict'])
-optim.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+with open(args.save, 'wb+') as f:
+    torch.save(model, f)
 test_acc, test_rae, test_corr, prediction_tensor  = evaluate(Data, Data.test[0], Data.test[1], model, evaluateL2, evaluateL1, args.batch_size)
 print ("test rse {:5.4f} | test rae {:5.4f} | test corr {:5.4f}".format(test_acc, test_rae, test_corr))
 numpy.set_printoptions(threshold=sys.maxsize)   # Makes it print the whole numpy array otherwise it will only print the start and end
