2b5850926cb29cd61c4185647cb58463fb53c62f
sava17 <37406543+sava17@users.noreply.github.com>
2020-05-06 10:32:31 +0200

    Updateret TAENet med dynamisk pooling variable

--- models/TAENet.py
+++ models/TAENet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 class Model(nn.Module):
     def __init__(self, args, data, cnn):
@@ -20,22 +21,43 @@ class Model(nn.Module):
         #self.encode = nn.Conv2d(1, self.hidC, kernel_size = (self.Ck, self.m)); # Kernel size = 6 * 8
         
         self.height_after_conv = (self.P - (self.Ck - 1))           # Conv layer changes shape by, Input size - (height - 1)
-        self.height_after_pooling = self.height_after_conv/2        # Max pooling 2x2 gives a input size reduction of input_size / 2
+        self.pooling_factor = 4
+        self.height_after_pooling = int(math.ceil(float(self.height_after_conv)/self.pooling_factor))        # Max pooling 2x2 gives a input size reduction of input_size / 2
         self.deconv_height = self.P - self.height_after_pooling + 1 # Transpose layer adds the height argument to the input shape -1, after convolution. So  
                                                                     # in order to get the original size of self.P, we find the difference between height after the
                                                                     # pooling layer and adds 1 to negate the -1 that is subtracted when using the method.
         
         #self.decode = nn.ConvTranspose2d(self.hidC, 1, (self.deconv_height, self.m))
-        self.pool = nn.MaxPool2d(2)
+        #self.pool = nn.MaxPool2d(1, self.pooling_factor)
+
+        #self.pool = nn.MaxPool2d(2)
         
 
         self.encode1 = nn.Conv2d(1, self.hidC, kernel_size = (self.Ck, self.m)); 
         self.encode2 = nn.Conv2d(self.hidC, 25, kernel_size = (self.Ck, 1)); 
         self.encode3 = nn.Conv2d(25, 12, kernel_size = (self.Ck, 1));
 
+
         self.decode3 = nn.ConvTranspose2d(12, 25, (self.Ck, 1))
         self.decode2 = nn.ConvTranspose2d(25, self.hidC, (self.Ck, 1))
         self.decode1 = nn.ConvTranspose2d(self.hidC, 1, (self.Ck, self.m))
+
+        #self.encoder = nn.Sequential(
+        #    nn.Conv2d(1, 16, 3, stride=3, padding=1),  # b, 16, 10, 10
+        #    nn.ReLU(True),
+        #    nn.MaxPool2d(2, stride=2),  # b, 16, 5, 5
+        #    nn.Conv2d(16, 8, 3, stride=2, padding=1),  # b, 8, 3, 3
+        #    nn.ReLU(True),
+        #    nn.MaxPool2d(2, stride=1)  # b, 8, 2, 2
+        #)
+        #self.decoder = nn.Sequential(
+        #    nn.ConvTranspose2d(8, 16, 3, stride=2),  # b, 16, 5, 5
+        #    nn.ReLU(True),
+        #    nn.ConvTranspose2d(16, 8, 5, stride=3, padding=1),  # b, 8, 15, 15
+        #    nn.ReLU(True),
+        #    nn.ConvTranspose2d(8, 1, 2, stride=2, padding=1),  # b, 1, 28, 28
+        #    nn.Tanh()
+        #)
 
 
     def forward(self, x):
@@ -44,7 +66,7 @@ class Model(nn.Module):
         ae = x.view(-1, 1, self.P, self.m)
 
         # CNN Autoencoder (3 layer)
-        ae = self.encode1(ae)   # width is 1 this layer, since 8 - (8 - 1) = 1
+        ae = self.encode1(ae)
         ae = self.encode2(ae)
         ae = self.encode3(ae)
         ae = self.decode3(ae)
