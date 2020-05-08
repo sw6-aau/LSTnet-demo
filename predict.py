b3a538a6a19983ed0cfcbfded333514234addde3
sava17 <37406543+sava17@users.noreply.github.com>
2020-05-08 16:03:41 +0200

    predict virker

--- predict.py
+++ predict.py
@@ -140,8 +140,11 @@ optim = Optim.Optim(
 
 # Load the best saved model.
 # Have changed the train- and validation to 0, so it testes on all the data
-with open(args.save, 'wb+') as f:
-    torch.save(model, f)
+with open(args.save, 'rb+') as f:
+    checkpoint = torch.load(f)
+model.load_state_dict(checkpoint['model_state_dict'])	
+optim.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+
 test_acc, test_rae, test_corr, prediction_tensor  = evaluate(Data, Data.test[0], Data.test[1], model, evaluateL2, evaluateL1, args.batch_size)
 print("test rse {:5.4f} | test rae {:5.4f} | test corr {:5.4f}".format(test_acc, test_rae, test_corr))
 
