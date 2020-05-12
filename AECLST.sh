b3d5b3b9dec299c63fbbe3a613050a136aec534a
sava17 <37406543+sava17@users.noreply.github.com>
2020-05-12 11:08:01 +0200

    Sh filer

---
+++ AECLST.sh
@@ -0,0 +1,1 @@
+python AECLST_main.py --gpu 0 --data data/exchange_rate.txt --save save/AECLST.pt --hidCNN 50 --hidRNN 50 --L1Loss False --output_fun None --epochs 1000 --output_fun None
---
+++ LSTNet.sh
@@ -0,0 +1,1 @@
+python main.py --gpu 0 --data data/exchange_rate.txt --save save/LSTNet.pt --hidCNN 50 --hidRNN 50 --L1Loss False --output_fun None --epochs 1000 --output_fun None
