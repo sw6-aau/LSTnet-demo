982ead9d680326ce695fc820031065933c0a26e0
sava17 <37406543+sava17@users.noreply.github.com>
2020-05-15 11:56:03 +0200

    fixed pooling error

--- models/AECLSTNet.py
+++ models/AECLSTNet.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import math
 
 class Model(nn.Module):
     def __init__(self, args, data, cnn, rnn, skip, activation):
@@ -13,7 +14,7 @@ class Model(nn.Module):
         self.hidS = skip;
         self.Ck = args.CNN_kernel;
         self.skip = args.skip;
-        self.pt = (self.P - self.Ck)/self.skip  # (168 - 6) / 24 = 6.75 rounded 6 # If they have made a mistake here it would make a lot more sense. Then 168 - 6 would be
+        self.pt = int((self.P - self.Ck)/self.skip)  # (168 - 6) / 24 = 6.75 rounded 6 # If they have made a mistake here it would make a lot more sense. Then 168 - 6 would be
                                                 # sequence length after being processed by CNN layer, but the actual shape after CNN layer was 163 (i.e. - 5), but that can be unintuitive to
                                                 # determine, since it is sequence length - (Ck - 1) after being processed by CNN layer not sequence length - Ck.
                                                 # This would make sense of the RNN-skip arguments, they want to feed the input to the RNN with the knoweledge
@@ -23,13 +24,15 @@ class Model(nn.Module):
 
         self.encode = nn.Conv2d(1, self.hidC, kernel_size = (self.Ck, self.m)); # 168, 8, -5 -7 = 163, 1
         
-        self.height_after_conv = (self.P - (self.Ck - 1))           # Conv layer changes shape by, Input size - (height - 1)
-        self.height_after_pooling = self.height_after_conv/2        # Max pooling 2x2 gives a input size reduction of input_size / 2
-        self.deconv_height = self.P - self.height_after_pooling + 1 # Transpose layer adds the height argument to the input shape -1, after convolution. So  
+        self.pooling_factor = 4
+        self.height_after_conv = (self.P - (self.Ck - 1))           # Conv layer changes shape by, Input size - (height - 1)  
+        self.height_after_pooling = int(math.ceil(float(self.height_after_conv)/self.pooling_factor))# Transpose layer adds the height argument to the input shape -1, after convolution. So  
                                                                     # in order to get the original size of self.P, we find the difference between height after the
                                                                     # pooling layer and adds 1 to negate the -1 that is subtracted when using the method.
-        self.decode = nn.ConvTranspose2d(self.hidC, 1, (self.Ck, self.m)) # 163, 1, 
-        self.pool = nn.MaxPool2d(1,4)
+        self.deconv_height = self.P - self.height_after_pooling + 1     
+        
+        self.decode = nn.ConvTranspose2d(self.hidC, 1, (self.deconv_height, self.m)) # 163, 1, 
+        self.pool = nn.MaxPool2d(1,self.pooling_factor)
 
 
         self.GRU1 = nn.GRU(self.hidC, self.hidR);
@@ -61,7 +64,7 @@ class Model(nn.Module):
         c = F.relu(self.encode(c))      # (128, 50, 163, 1)
         
         reconstructed = self.pool(c)                # (128, 50, 81, 1) (163 / 2 = 81, rounding down)
-        reconstructed = F.relu(self.decode(c))
+        reconstructed = F.relu(self.decode(reconstructed))
         reconstructed = torch.squeeze(reconstructed, 1);
 
         
